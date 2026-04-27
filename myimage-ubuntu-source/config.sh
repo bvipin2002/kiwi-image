f17f6ee252b1fd2d36d400c88136305619acf046
root <root@DESKTOP-JTC7BMM.localdomain>
2026-04-27 16:54:06 +0000

    new src

--- myimage-ubuntu-source/config.sh
+++ myimage-ubuntu-source/config.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "NPCI DevOps: Staging EFI bootloaders..."
+echo "NPCI DevOps: Staging EFI binaries..."
 
-# 1. Create the target directory inside the image root
+# 1. Create the standard directory structure
 mkdir -p /boot/efi/EFI/BOOT
 
-# 2. Copy the signed Shim (the primary boot entry)
-# We check both common signed locations
-if [ -f /usr/lib/shim/shimx64.efi.signed ]; then
-    cp /usr/lib/shim/shimx64.efi.signed /boot/efi/EFI/BOOT/BOOTX64.EFI
-    echo "Found signed shim."
-fi
+# 2. Copy the binaries that were just downloaded/installed via apt
+# These are the actual 'real' files that were missing before
+cp /usr/lib/shim/shimx64.efi.signed /boot/efi/EFI/BOOT/BOOTX64.EFI
+cp /usr/lib/grub/x86_64-efi-signed/grubx64.efi.signed /boot/efi/EFI/BOOT/grubx64.efi
 
-# 3. Copy MokManager (mmx64) - sometimes not .signed in Noble
-if [ -f /usr/lib/shim/mmx64.efi ]; then
-    cp /usr/lib/shim/mmx64.efi /boot/efi/EFI/BOOT/mmx64.efi
-elif [ -f /usr/lib/shim/mmx64.efi.signed ]; then
-    cp /usr/lib/shim/mmx64.efi.signed /boot/efi/EFI/BOOT/mmx64.efi
-fi
-
-# 4. Copy the signed GRUB
-if [ -f /usr/lib/grub/x86_64-efi-signed/grubx64.efi.signed ]; then
-    cp /usr/lib/grub/x86_64-efi-signed/grubx64.efi.signed /boot/efi/EFI/BOOT/grubx64.efi
-    echo "Found signed grub."
-fi
-
-# 5. Create the EFI-level grub.cfg
-# This points GRUB to the real config on the main Linux partition (gpt3)
+# 3. Create the EFI-level grub.cfg stub
 cat <<EOF > /boot/efi/EFI/BOOT/grub.cfg
-set prefix=(hd0,gpt3)/boot/grub
-configfile (hd0,gpt3)/boot/grub/grub.cfg
+search --no-floppy --file --set=root /boot/grub/grub.cfg
+set prefix=(\$root)/boot/grub
+configfile (\$root)/boot/grub/grub.cfg
 EOF
 
-echo "EFI staging complete."
-exit 0
+echo "Staging complete."
